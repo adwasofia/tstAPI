7f345583d0de2ba829d9dde18e6b3b981f852c0a
adwasofia <18220109@std.stei.itb.ac.id>
2022-12-07 20:40:43 +0700

    push draft (masih error)

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'data': 'blog list'}
+
+@app.get('/blog/{id}')
+def show(id):
+    # fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
